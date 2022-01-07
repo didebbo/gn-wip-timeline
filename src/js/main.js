850118e39920b3f96f1886a9cf66b7d3f6f70ab5
didebbo <gianluca.napoletano.93@gmail.com>
2022-01-07 19:29:09 +0100

    refactoring js

--- src/js/main.js
+++ src/js/main.js
@@ -2,46 +2,49 @@
 import '../scss/main.scss'
 import axios from 'axios';
 
-// Vars
-let url = "https://swapi.dev/api/planets";
-
 // DOM 
 const row = document.getElementById("row");
 row.innerHTML = "";
 
+// Vars
+let data = {
+    url: "https://swapi.dev/api/planets",
+    results: [],
+};
+
 // Generate Dynamic Data
-const genData = (row,url) => {
-    axios.get(url)
-        .then((response) => {
-            response.data.results.forEach(element => {
+const init = (data) => {
+    axios.get(data.url)
+    .then((response) => {
+        data.results = response.data.results;
+        data.results.forEach(element => {
 
-                const elName = element.name;
-                const elDate = element.created.split("T")[0];
-                const elTime = element.created.split("T")[1].split("Z")[0];
-                console.log(elName, elDate, elTime);
+            const elName = element.name;
+            const elDate = element.created.split("T")[0];
+            const elTime = element.created.split("T")[1].split("Z")[0];
+            console.log(elName, elDate, elTime);
 
-                const col = document.createElement("li");
-                const item = document.createElement("div");
-                const name = document.createElement("div");
-                const created = document.createElement("div");
-                const small = document.createElement("small");
+            const col = document.createElement("li");
+            const item = document.createElement("div");
+            const name = document.createElement("div");
+            const created = document.createElement("div");
+            const small = document.createElement("small");
 
-                col.classList.add("col");
-                item.classList.add("item");
-                name.classList.add("name");
-                created.classList.add("created");
+            col.classList.add("col");
+            item.classList.add("item");
+            name.classList.add("name");
+            created.classList.add("created");
 
-                name.innerHTML = elName;
-                small.innerHTML = elDate + " " + elTime.split(".")[0];
+            name.innerHTML = elName;
+            small.innerHTML = elDate + " " + elTime.split(".")[0];
 
-                created.appendChild(small);
-                item.appendChild(name);
-                item.appendChild(created);
-                col.appendChild(item);
-                row.appendChild(col);
-            });
+            created.appendChild(small);
+            item.appendChild(name);
+            item.appendChild(created);
+            col.appendChild(item);
+            row.appendChild(col);
         });
+    });
 }
 
-
-genData(row, url);
+init(data);
