d59d026fa18f674fcaa3e861c47307ea3ab0081c
didebbo <gianluca.napoletano.93@gmail.com>
2022-01-07 18:48:23 +0100

    refactoring js

--- src/js/main.js
+++ src/js/main.js
@@ -2,37 +2,46 @@
 import '../scss/main.scss'
 import axios from 'axios';
 
+// Vars
+let url = "https://swapi.dev/api/planets";
+
 // DOM 
 const row = document.getElementById("row");
 row.innerHTML = "";
 
-axios.get('https://swapi.dev/api/planets')
-.then((response) => {
-    response.data.results.forEach(element => {
+// Generate Dynamic Data
+const genData = (row,url) => {
+    axios.get(url)
+        .then((response) => {
+            response.data.results.forEach(element => {
 
-        const elName = element.name;
-        const elDate= element.created.split("T")[0];  
-        const elTime= element.created.split("T")[1].split("Z")[0];
-        console.log(elName,elDate,elTime);
+                const elName = element.name;
+                const elDate = element.created.split("T")[0];
+                const elTime = element.created.split("T")[1].split("Z")[0];
+                console.log(elName, elDate, elTime);
 
-        const col = document.createElement("li");
-        const item = document.createElement("div");
-        const name = document.createElement("div");
-        const created = document.createElement("div");
-        const small = document.createElement("small");
-        
-        col.classList.add("col");
-        item.classList.add("item");
-        name.classList.add("name");
-        created.classList.add("created");
+                const col = document.createElement("li");
+                const item = document.createElement("div");
+                const name = document.createElement("div");
+                const created = document.createElement("div");
+                const small = document.createElement("small");
 
-        name.innerHTML = elName;
-        small.innerHTML = elDate + " " + elTime.split(".")[0];
+                col.classList.add("col");
+                item.classList.add("item");
+                name.classList.add("name");
+                created.classList.add("created");
 
-        created.appendChild(small);
-        item.appendChild(name);
-        item.appendChild(created);
-        col.appendChild(item);
-        row.appendChild(col);
-    });
-});
+                name.innerHTML = elName;
+                small.innerHTML = elDate + " " + elTime.split(".")[0];
+
+                created.appendChild(small);
+                item.appendChild(name);
+                item.appendChild(created);
+                col.appendChild(item);
+                row.appendChild(col);
+            });
+        });
+}
+
+
+genData(row, url);
